bd6b907d1c0f8f40fffcb765a35448a58b8437d9
hrkrshnn <webmail.hari@gmail.com>
2021-09-15 11:59:13 +0200

    Changing the UserDefinedValueType::toString to `userdefined <name>`

--- test/libsolidity/syntaxTests/userDefinedValueType/explicit_conversions_err.sol
+++ test/libsolidity/syntaxTests/userDefinedValueType/explicit_conversions_err.sol
@@ -14,11 +14,11 @@ function f() pure {
     MyUInt.unwrap(AnotherUInt.wrap(10));
 }
 // ----
-// TypeError 9640: (99-109): Explicit type conversion not allowed from "int_const -1" to "user defined type MyUInt".
-// TypeError 9640: (115-128): Explicit type conversion not allowed from "int_const -1" to "user defined type MyAddress".
-// TypeError 9640: (134-143): Explicit type conversion not allowed from "int_const 5" to "user defined type MyUInt".
-// TypeError 9640: (149-170): Explicit type conversion not allowed from "address" to "user defined type MyAddress".
-// TypeError 9640: (177-204): Explicit type conversion not allowed from "user defined type MyUInt" to "user defined type AnotherUInt".
-// TypeError 9640: (210-238): Explicit type conversion not allowed from "user defined type AnotherUInt" to "user defined type MyUInt".
-// TypeError 9553: (263-277): Invalid type for argument in function call. Invalid implicit conversion from user defined type MyUInt to user defined type AnotherUInt requested.
-// TypeError 9553: (298-318): Invalid type for argument in function call. Invalid implicit conversion from user defined type AnotherUInt to user defined type MyUInt requested.
+// TypeError 9640: (99-109): Explicit type conversion not allowed from "int_const -1" to "userdefined MyUInt".
+// TypeError 9640: (115-128): Explicit type conversion not allowed from "int_const -1" to "userdefined MyAddress".
+// TypeError 9640: (134-143): Explicit type conversion not allowed from "int_const 5" to "userdefined MyUInt".
+// TypeError 9640: (149-170): Explicit type conversion not allowed from "address" to "userdefined MyAddress".
+// TypeError 9640: (177-204): Explicit type conversion not allowed from "userdefined MyUInt" to "userdefined AnotherUInt".
+// TypeError 9640: (210-238): Explicit type conversion not allowed from "userdefined AnotherUInt" to "userdefined MyUInt".
+// TypeError 9553: (263-277): Invalid type for argument in function call. Invalid implicit conversion from userdefined MyUInt to userdefined AnotherUInt requested.
+// TypeError 9553: (298-318): Invalid type for argument in function call. Invalid implicit conversion from userdefined AnotherUInt to userdefined MyUInt requested.
--- test/libsolidity/syntaxTests/userDefinedValueType/wrap_unwrap_assign_err.sol
+++ test/libsolidity/syntaxTests/userDefinedValueType/wrap_unwrap_assign_err.sol
@@ -4,5 +4,5 @@ function test() pure {
     function (int) returns (MyInt) g = MyInt.wrap;
 }
 // ----
-// TypeError 9574: (46-93): Type function (user defined type MyInt) pure returns (int256) is not implicitly convertible to expected type function (user defined type MyInt) returns (int256). Special functions can not be converted to function types.
-// TypeError 9574: (99-144): Type function (int256) pure returns (user defined type MyInt) is not implicitly convertible to expected type function (int256) returns (user defined type MyInt). Special functions can not be converted to function types.
+// TypeError 9574: (46-93): Type function (userdefined MyInt) pure returns (int256) is not implicitly convertible to expected type function (userdefined MyInt) returns (int256). Special functions can not be converted to function types.
+// TypeError 9574: (99-144): Type function (int256) pure returns (userdefined MyInt) is not implicitly convertible to expected type function (int256) returns (userdefined MyInt). Special functions can not be converted to function types.
